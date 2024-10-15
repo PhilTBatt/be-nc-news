72c5335efcb253d53370c6784662b23aac49ffb8
PhilTBatt <philtbatt@gmail.com>
2024-10-15 15:52:13 +0100

    Get article comments by article id

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,18 +1,25 @@
 const db = require('../db/connection')
-const { fetchArticleById, fetchArticles } = require('../models/articles.models')
+const { fetchArticleById, fetchArticles, fetchArticleComments } = require('../models/articles.models')
 
 exports.getArticleById = (req, res, next) => {
     const id = req.params.article_id
     return fetchArticleById(id)
-    .then(article => res.status(200).send({Article: article}))
-    .catch(err => next(err))
+    .then(article => res.status(200).send({article}))
+    .catch(next)
 }
 
 exports.getArticles = (req, res, next) => {
     return fetchArticles()
-    .then(articles  => res.status(200).send({Articles: articles}))
-    .catch((err) => {
-        console.log(err)
-        return next(err)
+    .then(articles => res.status(200).send({articles}))
+    .catch(next)
+}
+
+exports.getArticleComments = (req, res, next) => {
+    const id = req.params.article_id
+    fetchArticleById(id)
+    .then(() => {
+      return fetchArticleComments(id)
     })
+    .then(comments => res.status(200).send({comments}))
+    .catch(next)
 }
--- models/articles.models.js
+++ models/articles.models.js
@@ -21,3 +21,8 @@ exports.fetchArticles = () => {
         return articles.rows
     })
 }
+
+exports.fetchArticleComments = (id) => {
+    return db.query('SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC', [id])
+    .then((comments) => comments.rows)
+}
