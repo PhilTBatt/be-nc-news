105465ef019a2dc275c4ae98fd0dea2a9936a1fa
PhilTBatt <philtbatt@gmail.com>
2024-10-17 11:18:21 +0100

    Specific article has comment count

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,7 +1,9 @@
 const db = require('../db/connection')
 
 exports.fetchArticleById = (id) => {
-    return db.query('SELECT * FROM articles WHERE article_id = $1', [id])
+    return db.query(`SELECT articles.article_id, articles.title, articles.author, articles.topic, articles.body, articles.created_at, 
+        articles.votes, articles.article_img_url, COUNT(comments.article_id)::int AS comment_count FROM articles 
+        LEFT JOIN comments ON comments.article_id = articles.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id`, [id])
     .then(article => {
         if (article.rows.length === 0) {
             return Promise.reject({status: 404, msg: 'Article not found'})
