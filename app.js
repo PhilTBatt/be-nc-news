99e2e375843624d72e0a23c4fc9f565ffa9d0121
PhilTBatt <philtbatt@gmail.com>
2024-10-18 14:49:03 +0100

    Paginates articles

--- app.js
+++ app.js
@@ -20,8 +20,6 @@ app.use((err, req, res, next) => {
         res.status(404).send({msg: 'User not found'})
     } else if (err.code === '23503' && err.constraint === 'comments_article_id_fkey') {
         res.status(404).send({msg: 'Article not found'})
-    } else if (err.code === '42703') {
-        res.status(400).send({msg: `Invalid ${err.context}_id`})
     } else if (err.status && err.msg) {
         res.status(err.status).send({msg: err.msg})
     } else {
--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -8,9 +8,12 @@ exports.getArticleById = (req, res, next) => {
 }
 
 exports.getArticles = (req, res, next) => {
-    const {sort_by: sortBy, order, topic} = req.query
-    return fetchArticles(sortBy, order, topic)
-    .then(articles => res.status(200).send({articles}))
+    const {sort_by: sortBy, order, topic, limit, p} = req.query
+    return fetchArticles(sortBy, order, topic, limit, p)
+    .then(([articles, countResult]) => {
+        const total_count = Number(countResult.rows[0].total_count)
+        res.status(200).send({articles, total_count})
+    })
     .catch(next)
 }
 
