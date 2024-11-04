0ac4b6eab25fdce8943e7c7f2b84bc7594e2a9db
PhilTBatt <philtbatt@gmail.com>
2024-11-04 09:49:07 +0000

    Added Cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 
 const apiRouter = require('./routes/api-router')
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/api', apiRouter)
